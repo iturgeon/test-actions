8f4b153326a18df89d23a7ba5fccddd31181106e
iturgeon <iturgeon@gmail.com>
2020-10-02 19:33:13 -0400

    whatever

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const x = (test) => {
-  return 500 * 2;
-};
+
+
+
+const x=(test) => {
+  return (500 * 2)
+}
 
 if (x() * 2 > 99) {
-  console.log(5000);
+   			  console.log(5000)
+
+
 }
+
+console.log(              'yeeeeeeeeeeeeeeeeee'    )
