95fdaffc39f282294116df69a5b04eaa9893624d
iturgeon <iturgeon@users.noreply.github.com>
2020-10-02 23:34:38 +0000

    Automated prettier format changes

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-
-
-
-const x=(test) => {
-  return (500 * 2)
-}
+const x = (test) => {
+  return 500 * 2;
+};
 
 if (x() * 2 > 99) {
-   			  console.log(5000)
-
-
+  console.log(5000);
 }
 
-console.log(              'yeeeeeeeeeeeeeeeeee'    )
+console.log("yeeeeeeeeeeeeeeeeee");
